5ecbdceab491b16054b212f87398e48bf4692b7a
Jek <varvar123456789018@gmail.com>
2026-03-31 15:20:52 +0300

    Изменена папочная структура согласно требованиям

--- ConverterValute/ValueConverter/natives/cpp/DataTxt.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <string>
-
-void EnteringString(std::string& txt);
-std::string GetText();
---
+++ natives/cpp/DataTxt.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <string>
+
+void EnteringString(std::string& txt);
+std::string GetText();
